dba94183bb8c3287b26c37560c0a6222a520b915
Tim Wang <timwyc@gmail.com>
2022-02-19 20:13:52 +0100

    Added functioning deck builder to DeckOfCards class
    Generates a range of numbers and in the init() method spins up the whole deck into objects and stores that in a list.

--- pylackjack.py
+++ pylackjack.py
@@ -1,15 +1,27 @@
 from deckofcards import DeckOfCards
 
-print("\nWelcome to the Blackjack game!")
-gamestart = input("\nDo you want to play? (y/n?) ")
-if gamestart == "y":
-    the_deck = DeckOfCards()
-    #print("let's pretend you have some cards")
-    player_choice = input("\nThere is a deck of cards in front of you, you can \"draw\" to get a card or anything else to exit the game.")
-    while player_choice == "draw":
-        card = the_deck.draw()
-        print(f"You drew a {card}!\n")
-        player_choice = input("What next?")
-    print("Thanks for playing!")
-else:
-    print("Cya Space Cowboy")
+the_deck = DeckOfCards()
+print(f"count is {the_deck.count()}")
+print(the_deck.draw())
+
+# def func(x):
+#     return x*2
+
+# decklist = [func(x) for x in range(1,53)]
+
+# print(decklist)           
+
+# print("\nWelcome to the Blackjack game!")
+# gamestart = input("\nDo you want to play? (y/n?) ")
+# if gamestart == "y":
+#     the_deck = DeckOfCards()
+#     #print("let's pretend you have some cards")
+#     player_choice = input("\nThere is a deck of cards in front of you, you can \"draw\" to get a card or anything else to exit the game.")
+#     while player_choice == "draw":
+#         card = the_deck.draw()
+#         print(f"You drew a {card}!\n")
+#         player_choice = input("What next?")
+#     print("Thanks for playing!")
+# else:
+#     print("Cya Space Cowboy")
+
