eaeb4103b9b0c0ab185718095ec904cbc86c5ca6
Tim Wang <timwyc@gmail.com>
2022-02-19 20:26:06 +0100

    cleaned up draw method in deck building class
    Also cards are not objects (or maybe they are technically) but the immediate term is dictionary as defined by { } brackets

--- deckofcards.py
+++ deckofcards.py
@@ -2,26 +2,22 @@ import random
 
 class DeckOfCards:
     def __init__(self):
+        """generates the cards for the deck"""
         def func(value_num, suit_num): #cardvaule is 1 - 12, suit is 1 - 4
             card_value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
             card_suit = ["S", "H", "D", "C"]
             card = {
                     "id" : value_num,
+                    "name" : f"{card_value[value_num]} {card_suit[suit_num]}",
                     "value" : card_value[value_num],
                     "suit" : card_suit[suit_num],
                 }
             return card
-            # f"{card_value[value_num]}, {card_suit[suit_num]}"
-            if x % 4 == 1:
-                return x*2
-        #self.deck = [x for x in range(1,53)]
         self.deck = [func(x % 13 , x // 13) for x in range(52)]
-        #print(self.deck)
-        
-        #self.deck = range(52)
     
     def draw(self):
-        return random.choice(self.deck)
+        """by default returns the name of the card dictionary"""
+        return random.choice(self.deck)["name"]
 
     def count(self):
         return len(self.deck)
--- pylackjack.py
+++ pylackjack.py
@@ -1,27 +1,20 @@
 from deckofcards import DeckOfCards
 
-the_deck = DeckOfCards()
-print(f"count is {the_deck.count()}")
-print(the_deck.draw())
+print("\nWelcome to the Blackjack game!")
 
-# def func(x):
-#     return x*2
+gamestart = input("\nDo you want to play? (y/n?) ")
 
-# decklist = [func(x) for x in range(1,53)]
+if gamestart == "y":
+    the_deck = DeckOfCards()
+    player_choice = input("\nThere is a deck of cards in front of you, you can type \"draw\" \n(any other input exits the game): ")
+    while player_choice == "draw":
+        card = the_deck.draw()
+        print(f"You drew a {card}!\n")
+        player_choice = input("What next? ")
+    print("Thanks for playing!")
+else:
+    print("Cya Space Cowboy")
 
-# print(decklist)           
-
-# print("\nWelcome to the Blackjack game!")
-# gamestart = input("\nDo you want to play? (y/n?) ")
-# if gamestart == "y":
-#     the_deck = DeckOfCards()
-#     #print("let's pretend you have some cards")
-#     player_choice = input("\nThere is a deck of cards in front of you, you can \"draw\" to get a card or anything else to exit the game.")
-#     while player_choice == "draw":
-#         card = the_deck.draw()
-#         print(f"You drew a {card}!\n")
-#         player_choice = input("What next?")
-#     print("Thanks for playing!")
-# else:
-#     print("Cya Space Cowboy")
-
+# the_deck = DeckOfCards() 
+# print(f"count is {the_deck.count()}")
+# print(the_deck.draw())
