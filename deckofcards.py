95cb6ebf724f5cabbcc172b3627aa4955a45a6e1
Tim Wang <timwyc@gmail.com>
2022-02-21 20:38:24 +0100

    Commit before shifting focus to data viz project

--- deckofcards.py
+++ deckofcards.py
@@ -15,10 +15,13 @@ class DeckOfCards:
             return False
 
     def count(self):
+        """
+        Returns the number of cards left in the deck
+        """
         return len(self.deck)
     
-    def func(self, value_num, suit_num): #cardvaule is 1 - 12, suit is 1 - 4
-        """generates each card for the deck"""
+    def func(self, value_num, suit_num): #value_num is 1 - 12, suit_num is 1 - 4
+        """Generates each card for the deck"""
         card_value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
         card_suit = ["\u2660", "\u2665", "\u2666", "\u2663"]
         card = {
