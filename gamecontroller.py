e2dfe8ac624ab432a8b2ac77e05ed2bc485e3a78
Tim Wang <timwyc@gmail.com>
2022-03-13 18:03:14 +0100

    Move code into a main method + other minor cleanups
    Cleaned up a bunch of stuff and experimented with trying to get unit tests to run every time I run the program.

--- gamecontroller.py
+++ gamecontroller.py
@@ -5,20 +5,24 @@ class GameController:
     def __init__(self, bankroll = 500):
         self.game_over = False
         self.bankroll = bankroll
-            
+        self.player_base = ["player", "dealer"]
 
-    def moneyCount(self):
+    def get_bankroll(self):
         return self.bankroll
+
+    def next_player(self):
+        return self.player_base
 
 
 
 ##
 # Below are temp things to verify functionalities while I build
 ##
+
 face = GameController()
 
-print(face.moneyCount())
+print(face.get_bankroll())
 
 face1 = GameController(1000)
 
-print(face1.moneyCount())
+print(face1.get_bankroll())
--- pylackjack.py
+++ pylackjack.py
@@ -1,44 +1,51 @@
 from calendar import TUESDAY
 from deckofcards import DeckOfCards
+#from test_deck_of_cards import TestDeckOfCards
 
-print("\nWelcome to the Blackjack game!")
+def main():
 
-#       BELOW CODE IS FOR PROPER GAME FORMAT
-# gamestart = input("\nDo you want to play? (y/n?) ")
-#
-# if gamestart == "y":
-#     the_deck = DeckOfCards()
-#     player_choice = input("\nThere is a deck of cards in front of you, you can type \"draw\" \n(any other input exits the game): ")
-#     while player_choice == "draw":
-#         card = the_deck.draw()
-#         print(f"You drew a {card}!\n")
-#         player_choice = input("What next? ")
-#     print("Thanks for playing!")
-# else:
-#     print("Cya Space Cowboy")
+    print("yoyo")
 
+    print("\nWelcome to the Blackjack game!")
 
-player_hand = []
-player_choice = ""
-the_deck = DeckOfCards() 
-while player_choice != "q":
-
-    if player_choice == "count":
-        print(the_deck.count())    
-
-    card = the_deck.draw()
-    if card != False:
-        player_hand.append(card)
-        print(f"\nYou drew a {card}!")
-    
-    if player_hand == False:
-        print("\nYour hand is currently empty")
-    else:
-        print(f"Your had is: ")
-        for card in player_hand:
-            print(f"{card}", end="  ")
-    player_choice = input("\nagain? ")
+    #       BELOW CODE IS FOR PROPER GAME FORMAT
+    # gamestart = input("\nDo you want to play? (y/n?) ")
+    #
+    # if gamestart == "y":
+    #     the_deck = DeckOfCards()
+    #     player_choice = input("\nThere is a deck of cards in front of you, you can type \"draw\" \n(any other input exits the game): ")
+    #     while player_choice == "draw":
+    #         card = the_deck.draw()
+    #         print(f"You drew a {card}!\n")
+    #         player_choice = input("What next? ")
+    #     print("Thanks for playing!")
+    # else:
+    #     print("Cya Space Cowboy")
 
 
-# print(f"count is {the_deck.count()}")
-# print(the_deck.draw())
+    player_hand = []
+    player_choice = ""
+    the_deck = DeckOfCards() 
+    while player_choice != "q":
+
+        if player_choice == "count":
+            print(the_deck.count())    
+
+        card = the_deck.draw()
+        if card != False:
+            player_hand.append(card)
+            print(f"\nYou drew a {card}!")
+        
+        if player_hand == False:
+            print("\nYour hand is currently empty")
+        else:
+            print(f"Your had is: ")
+            for card in player_hand:
+                print(f"{card}", end="  ")
+        player_choice = input("\nagain? ")
+
+
+    # print(f"count is {the_deck.count()}")
+    # print(the_deck.draw())
+if __name__ == '__main__':
+    main()
